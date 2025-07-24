d437a479bd02b92c99616b12396824b4c68393e2
SeongChoel <tldtlddl1234@gmail.com>
2025-07-24 16:30:45 +0900

    feat: User 엔티티 작성

---
+++ src/main/java/com/back/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.back.domain.user.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PRIVATE)
+    @EqualsAndHashCode.Include
+    private long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Min(0)
+    private int exp; //경험치
+
+    @Min(1)
+    private int level; //레벨
+
+    @Column(nullable = false, unique = true)
+    private String apiKey; //리프레시 토큰
+
+    @Column(nullable = false)
+    private boolean isAdmin;
+
+    // 유저가 푼 퀴즈 기록을 저장하는 리스트 일단 엔티티 없어서 주석
+   //@OneToMany(mappedBy ="user", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<UserQuizAnswer> userQuizAnswers = new ArrayList<>(); //유저가 퀴즈를 푼 기록
+
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+}
