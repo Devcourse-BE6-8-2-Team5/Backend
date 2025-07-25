c306c8e00f5b7ee69d928d76312e4ddfd2a68866
SeongChoel <tldtlddl1234@gmail.com>
2025-07-25 12:42:54 +0900

    feat: 회원 로그아웃, 회원 내정보조회, DTO 리팩토링

---
+++ src/main/java/com/back/domain/member/dto/MemberWithAuthDto.java
@@ -0,0 +1,29 @@
+package com.back.domain.member.dto;
+
+import com.back.domain.member.entity.Member;
+import lombok.Getter;
+
+
+@Getter
+public class MemberWithAuthDto {
+    private Long id;
+    private String name;
+    private String email;
+    private boolean isAdmin;
+
+    public MemberWithAuthDto(Long id, String email, String name, boolean isAdmin) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.isAdmin = false;
+    }
+
+    public MemberWithAuthDto(Member member) {
+        this.id = member.getId();
+        this.email = member.getEmail();
+        this.name = member.getName();
+        this.isAdmin = member.isAdmin();
+    }
+}
+
+
