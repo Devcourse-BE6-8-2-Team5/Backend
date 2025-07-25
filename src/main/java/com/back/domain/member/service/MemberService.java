c5a65b2ba9715ffa306c8d649a316e3068c8243a
SeongChoel <83538651+SeongChoel@users.noreply.github.com>
2025-07-25 17:47:53 +0900

    [feat] : 회원정보 수정#1 (#59)
    * feat : 회원수정 기능구현
    
    * refactor:회원수정 리팩토링
    
    ---------
    
    Co-authored-by: SeongChoel <tldtlddl1234@gmail.com>

--- src/main/java/com/back/domain/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -35,5 +36,13 @@ public class MemberService {
 
     public Optional<Member> findById(long id) {
         return memberRepository.findById(id);
+    }
+
+    @Transactional
+    public void modify(Member member, String name, String password, String email) {
+        member.setName(name);
+        member.setPassword(password);
+        member.setEmail(email);
+        memberRepository.save(member);
     }
 }
