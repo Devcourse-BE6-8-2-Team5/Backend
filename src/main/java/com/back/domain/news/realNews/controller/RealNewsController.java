3eb7dec561475f928b94644a433ac18eed741420
seungwookc97 <sochoi0832@gmail.com>
2025-07-25 09:25:24 +0900

    refactor: 엔드포인트 변경

--- src/main/java/com/back/domain/news/realNews/controller/RealNewsController.java
+++ src/main/java/com/back/domain/news/realNews/controller/RealNewsController.java
@@ -2,25 +2,20 @@ package com.back.domain.news.realNews.controller;
 
 import com.back.domain.news.realNews.dto.NaverNewsDto;
 import com.back.domain.news.realNews.dto.RealNewsDto;
-import com.back.domain.news.realNews.entity.RealNews;
 import com.back.domain.news.realNews.service.RealNewsService;
 import com.back.global.rsData.RsData;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/real-news")
+@RequestMapping("real-news")
 @RequiredArgsConstructor
-public class ApiV1RealNewsController {
+public class RealNewsController {
     private final RealNewsService realNewsService;
 
-    //추후 삭제예정
-    //http://localhost:8080/api/v1/real-news/fetch?query="삼성전자" << 이런 식으로 뉴스 추출 가능
+    //추후 삭제예정 fetch 테스트
     @GetMapping("/fetch")
     public RsData<List<NaverNewsDto>> fetchNewsByQuery(@RequestParam String query) {
         List<NaverNewsDto> newsList = realNewsService.fetchNews(query);
