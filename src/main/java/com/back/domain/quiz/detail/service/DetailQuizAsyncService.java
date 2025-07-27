3ec0b2ce115b9077b71454614c9b761d3777a79d
dbfgml20 <dbfgml20@gmail.com>
2025-07-28 00:37:02 +0900

    refactor : 코드 구조 개선

--- src/main/java/com/back/domain/quiz/detail/service/DetailQuizAsyncService.java
+++ src/main/java/com/back/domain/quiz/detail/service/DetailQuizAsyncService.java
@@ -20,25 +20,30 @@ public class DetailQuizAsyncService {
 
     @Async("quizExecutor")
     public CompletableFuture<Void> generateAsync(long newsId) {
-        if(inProgressMap.containsKey(newsId)){
+        if (inProgressMap.putIfAbsent(newsId, Boolean.TRUE) != null) {
             log.warn("이미 진행 중인 퀴즈 생성 작업이 있습니다. 뉴스 ID: " + newsId);
             return CompletableFuture.completedFuture(null); // 이미 진행 중인 작업이 있으면 바로 반환
         }
 
-        return CompletableFuture.runAsync(() -> {
-            try {
-                // Rate limit 적용하여 Ai 호출해 퀴즈 생성(트랜잭션 없음)
-                List<DetailQuizDto> quizzes = detailQuizRateLimitedService.generatedQuizzesWithRateLimit(newsId);
+        CompletableFuture<Void> result;
+        try {
+            // Rate limit 적용하여 Ai 호출해 퀴즈 생성(트랜잭션 없음)
+            List<DetailQuizDto> quizzes = detailQuizRateLimitedService.generatedQuizzesWithRateLimit(newsId);
 
-                // 생성된 퀴즈를 DB에 저장(트랜잭션)
-                detailQuizService.saveQuizzes(newsId, quizzes);
+            // 생성된 퀴즈를 DB에 저장(트랜잭션)
+            detailQuizService.saveQuizzes(newsId, quizzes);
 
-                log.info("상세 퀴즈 생성 완료, 뉴스 ID: " + newsId);
-            } catch (Exception e) {
-                log.error("[실패] 뉴스 퀴즈 생성 실패 - newsId: {}, 오류: {}", newsId, e.getMessage(), e);
-            } finally {
-                inProgressMap.remove(newsId);
-            }
-        });
+            log.info("상세 퀴즈 생성 완료, 뉴스 ID: " + newsId);
+            result = CompletableFuture.completedFuture(null);
+
+        } catch (Exception e) {
+            log.error("[실패] 뉴스 퀴즈 생성 실패 - newsId: {}, 오류: {}", newsId, e.getMessage(), e);
+            result = CompletableFuture.completedFuture(null);
+
+        } finally {
+            inProgressMap.remove(newsId);
+        }
+
+        return result;
     }
 }
