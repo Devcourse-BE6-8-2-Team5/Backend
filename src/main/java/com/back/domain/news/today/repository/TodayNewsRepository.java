52a68e1249c29f9654dab0576753af520093552f
dbfgml20 <dbfgml20@gmail.com>
2025-07-29 14:36:48 +0900

    fix : repository 중복 문제 수정

--- src/main/java/com/back/domain/news/today/repository/TodayNewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.back.domain.news.today.repository;
-
-import com.back.domain.news.today.entity.TodayNews;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodayNewsRepository extends JpaRepository<TodayNews, Long> {
-}
