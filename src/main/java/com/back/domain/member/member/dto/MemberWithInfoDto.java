39686277de963539ae37a935e26da424457097d2
SeongChoel <tldtlddl1234@gmail.com>
2025-07-29 10:14:39 +0900

    feat: 레벨에 따른 캐릭터 이미지 dto로 반환해서줌

--- src/main/java/com/back/domain/member/member/dto/MemberWithInfoDto.java
+++ src/main/java/com/back/domain/member/member/dto/MemberWithInfoDto.java
@@ -1,6 +1,7 @@
 package com.back.domain.member.member.dto;
 
 import com.back.domain.member.member.entity.Member;
+import com.back.global.util.LevelSystem;
 import lombok.Getter;
 
 
@@ -13,6 +14,7 @@ public class MemberWithInfoDto {
     private int exp;
     private int level;
     private String role;
+    private String characterImage; // 레벨에 따른 캐릭터 이미지
 
 
     public MemberWithInfoDto(Member member) {
@@ -20,7 +22,8 @@ public class MemberWithInfoDto {
         this.name = member.getName();
         this.email = member.getEmail();
         this.exp = member.getExp();
-        this.level = member.getLevel();
+        this.level = LevelSystem.calculateLevel(member.getExp());
+        LevelSystem.getImageByLevel(level);
         this.role = member.getRole();
     }
 }
