5df94fe98bf6b577b4479a6c1b163369c82d625b
dbfgml20 <dbfgml20@gmail.com>
2025-07-30 15:15:55 +0900

    refactor : 불필요한 생성자 제거

--- src/main/java/com/back/domain/quiz/detail/dto/DetailQuizUpdateReqDto.java
+++ src/main/java/com/back/domain/quiz/detail/dto/DetailQuizUpdateReqDto.java
@@ -1,6 +1,5 @@
 package com.back.domain.quiz.detail.dto;
 
-import com.back.domain.quiz.detail.entity.DetailQuiz;
 import com.back.domain.quiz.detail.entity.Option;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,14 +11,5 @@ public record DetailQuizUpdateReqDto(
         @NotBlank String option3,
         @NotNull Option correctOption
 ) {
-    public DetailQuizUpdateReqDto(DetailQuiz detailQuiz) {
-        this(
-                detailQuiz.getQuestion(),
-                detailQuiz.getOption1(),
-                detailQuiz.getOption2(),
-                detailQuiz.getOption3(),
-                detailQuiz.getCorrectOption()
-        );
-    }
 }
 
