6158805b1c58e9f911d73b8849b9ba9416f945cb
yeeJin227 <dpwls99600@gmail.com>
2025-07-31 15:33:34 +0900

    refactor: swagger, h2 경로 제한 설정 완료

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -51,7 +51,7 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
         String uri = request.getRequestURI();
 
         // Swagger, H2 콘솔 등의 경로는 바로 필터 통과시키기
-        if (uri.startsWith("/swagger-ui/") || uri.startsWith("/v3/api-docs/") || uri.startsWith("/swagger-resources/") || uri.startsWith("/h2-console/")) {
+        if (uri.startsWith("/swagger-ui/") || uri.startsWith("/v3/api-docs/") || uri.startsWith("/swagger-resources/") || uri.startsWith("/h2-console")) {
             filterChain.doFilter(request, response);
             return;
         }
