2e0bffc19a3525d00878b55f34ff1d0a719f3822
dbfgml20 <dbfgml20@gmail.com>
2025-07-31 11:49:45 +0900

    팩트 퀴즈 테스트코드 작성

--- src/test/java/com/back/backend/domain/quiz/fact/controller/FactQuizControllerTest.java
+++ src/test/java/com/back/backend/domain/quiz/fact/controller/FactQuizControllerTest.java
@@ -20,10 +20,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -73,5 +73,129 @@ public class FactQuizControllerTest {
                 .andExpect(jsonPath("$.code").value(200))
                 .andExpect(jsonPath("$.message").value("팩트 퀴즈 목록 조회 성공"))
                 .andExpect(jsonPath("$.data.length()").value(quizCount));
+    }
+
+    @Test
+    @DisplayName("GET /api/quiz/fact/category - 뉴스 카테고리별 팩트 퀴즈 목록 조회 성공")
+    void t2() throws Exception {
+        //Given
+        String category = "IT";
+
+        //When
+        ResultActions resultActions = mvc.perform(get("/api/quiz/fact/category")
+                .param("category", category) // IT 카테고리로 조회
+        ).andDo(print());
+
+        //Then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("getFactQuizzesByCategory"))
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.message").value("팩트 퀴즈 목록 조회 성공. 카테고리: " + category))
+                .andExpect(jsonPath("$.data.length()").value(2))
+                .andExpect(jsonPath("$.data[0].id").isNumber())
+                .andExpect(jsonPath("$.data[0].realNewsTitle").value("인도 최대 IT 서비스 TCS 1만2200명 감원 계획"))
+                .andExpect(jsonPath("$.data[0].question").isString());
+
+    }
+
+    // 팩트 퀴즈 서비스 로직 수정 후 테스트
+//    @Test
+//    @DisplayName("GET /api/quiz/fact/category - 뉴스 카테고리별 팩트 퀴즈 목록 조회 실패 (존재하지 않는 카테고리)")
+//    void t3() throws Exception {
+//        //Given
+//        String category = "SUMMER"; // 존재하지 않는 카테고리
+//
+//        //When
+//        ResultActions resultActions = mvc.perform(get("/api/quiz/detail/category/")
+//                .param("category", category)
+//        ).andDo(print());
+//
+//        //Then
+//        resultActions
+//                .andExpect(status().isBadRequest())
+//                .andExpect(handler().methodName("getFactQuizzesByCategory"))
+//                .andExpect(jsonPath("$.code").value(400))
+//                .andExpect(jsonPath("$.message").value("팩트 퀴즈 목록 조회 성공. 카테고리: " + category));
+//
+//    }
+
+    @Test
+    @DisplayName("GET /api/quiz/fact/{id} - 팩트 퀴즈 단건 조회 성공")
+    void t4() throws Exception {
+        //Given
+        Long quizId = 3L;
+
+        //When
+        ResultActions resultActions = mvc.perform(get("/api/quiz/fact/" + quizId)
+        ).andDo(print());
+
+        //Then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("getFactQuizById"))
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.message").value("팩트 퀴즈 조회 성공. ID: " + quizId))
+                .andExpect(jsonPath("$.data.id").isNumber())
+                .andExpect(jsonPath("$.data.question").isString())
+                .andExpect(jsonPath("$.data.realNewsTitle").value("경제계 \"상법·노조법 개정안 국회 급물살, 우려 넘어 참담\""))
+                .andExpect(jsonPath("$.data.realNewsContent", startsWith("한경협·대한상의 등 경제8단체 공동입장문 \"국회, 연이은 규제 입법…")))
+                .andExpect(jsonPath("$.data.fakeNewsContent", startsWith("국회에서 논의 중이던 상법 및 노동조합법 개정안이 경제계의 거센 반발에도")))
+                .andExpect(jsonPath("$.data.correctNewsType").exists());
+    }
+
+    @Test
+    @DisplayName("GET /api/quiz/fact/{id} - 팩트 퀴즈 단건 조회 실패 - 존재하지 않는 ID")
+    void t5() throws Exception {
+        //Given
+        Long quizId = 999L;
+
+        //When
+        ResultActions resultActions = mvc.perform(get("/api/quiz/fact/" + quizId)
+        ).andDo(print());
+
+        //Then
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(handler().methodName("getFactQuizById"))
+                .andExpect(jsonPath("$.code").value(404))
+                .andExpect(jsonPath("$.message").value("팩트 퀴즈를 찾을 수 없습니다. ID: " + quizId));
+
+    }
+
+    @Test
+    @DisplayName("DELETE /api/quiz/fact/{id} - 팩트 퀴즈 삭제 성공")
+    void t6() throws Exception {
+        //Given
+        Long quizId = 1L;
+
+        //When
+        ResultActions resultActions = mvc.perform(delete("/api/quiz/fact/{id}", quizId)
+        ).andDo(print());
+
+        //Then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("deleteFactQuiz"))
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.message").value("팩트 퀴즈 삭제 성공. ID: " + quizId));
+    }
+
+    @Test
+    @DisplayName("DELETE /api/quiz/fact/{id} - 팩트 퀴즈 삭제 실패 - 존재하지 않는 ID")
+    void t7() throws Exception {
+        //Given
+        Long quizId = 999L;
+
+        //When
+        ResultActions resultActions = mvc.perform(delete("/api/quiz/fact/{id}", quizId)
+        ).andDo(print());
+
+        //Then
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(handler().methodName("deleteFactQuiz"))
+                .andExpect(jsonPath("$.code").value(404))
+                .andExpect(jsonPath("$.message").value("팩트 퀴즈를 찾을 수 없습니다. ID: " + quizId));
     }
 }
