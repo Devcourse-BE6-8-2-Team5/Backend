4f5cd4106a64a46a85f281e91281aff7d026b8dd
dbfgml20 <53211374+dbfgml2000@users.noreply.github.com>
2025-08-01 12:35:59 +0900

    fix : DailyQuizService 오류 해결 (#180)

--- src/main/java/com/back/domain/quiz/daily/service/DailyQuizService.java
+++ src/main/java/com/back/domain/quiz/daily/service/DailyQuizService.java
@@ -19,7 +19,6 @@ import com.back.domain.quiz.detail.entity.Option;
 import com.back.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -79,8 +78,7 @@ public class DailyQuizService {
         );
     }
 
-
-    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
+    @Transactional
     public void createDailyQuiz() {
         TodayNews todayNews = todayNewsRepository.findFirstByOrderBySelectedDateDesc()
                 .orElseThrow(() -> new ServiceException(404, "오늘의 뉴스가 없습니다."));
