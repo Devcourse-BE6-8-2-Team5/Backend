8b711daadea86ffa9bbf87814f162456354da528
seungwookc97 <sochoi0832@gmail.com>
2025-08-04 09:46:25 +0900

    [feat] 뉴스 컨트롤러 테스트 작성 (#187)
    * feat: 뉴스 분석 과정에서 비동기화, rate limiter 도입
    
    * feat:가짜 뉴스 생성 비동기 처리
    
    * feat:네이버 api fetch 작업 비동기 처리
    
    * fix: 가짜뉴스 생성 이중 비동기 처리 수정
    
    * fix: 2차 비동기 수정 및 rate limiter time out 도입
    
    * fix:하드코딩 수정
    
    * feat: admin 컨트롤러 테스트 작성
    
    * fix: 경로 수정

--- src/main/java/com/back/global/ai/processor/FakeNewsGeneratorProcessor.java
+++ src/main/java/com/back/global/ai/processor/FakeNewsGeneratorProcessor.java
@@ -64,8 +64,7 @@ public class FakeNewsGeneratorProcessor implements AiRequestProcessor<FakeNewsDt
            %s
            
            **중요: 반드시 정확한 ID 사용**
-           realNewsId는 반드시 %s을 사용해야 합니다. 다른 숫자를 사용하지 마세요
-           
+           realNewsId는 반드시 %s을 사용해야 합니다. 다른 숫자를 사용하지 마세요  
            **원본과 비슷한 분량으로 작성하세요**
            - 관련 문장들: 붙여서 작성
            - 주제 바뀔 때: 반드시 \\\\n\\\\n 사용
