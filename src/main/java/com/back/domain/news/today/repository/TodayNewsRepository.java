0c159ed2d2670d9c97bea752dc84911448de2f90
seungwookc97 <sochoi0832@gmail.com>
2025-08-05 10:39:19 +0900

    refactor : deletebySelectedDate >  clearAutomatically 추가

--- src/main/java/com/back/domain/news/today/repository/TodayNewsRepository.java
+++ src/main/java/com/back/domain/news/today/repository/TodayNewsRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface TodayNewsRepository extends JpaRepository<TodayNews, Long> {
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Transactional
     void deleteBySelectedDate(LocalDate today);
 
